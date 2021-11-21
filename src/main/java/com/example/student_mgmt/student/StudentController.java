c7d5dbb5684d5d4ccd5eb5c38181f71cb2b829e3
vule1997 <nemanja.vuletic97@gmail.com>
2021-11-21 02:49:02 +0100

    added html

--- src/main/java/com/example/student_mgmt/student/StudentController.java
+++ src/main/java/com/example/student_mgmt/student/StudentController.java
@@ -1,15 +1,15 @@
 package com.example.student_mgmt.student;
 
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @AllArgsConstructor
 @RequestMapping(path = "api")
 public class StudentController {
@@ -17,7 +17,7 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping(path = "new")
-    public String add(Student student) {
+    public void add(Student student) {
         Student student1 = new Student ("Ime1", "Prezime1", "mail1@gmail.com");
         Student student2 = new Student ("Ime2", "Prezime2", "mail2@gmail.com");
         Student student3 = new Student ("Ime3", "Prezime3", "mail3@gmail.com");
@@ -26,8 +26,6 @@ public class StudentController {
         studentService.add(student2);
         studentService.add(student3);
         studentService.add(student4);
-
-        return "Students saved";
 
     }
 
